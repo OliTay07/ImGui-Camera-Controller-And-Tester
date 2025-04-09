f0e928097105206e048adea0e86cce68c768ee1c
OliviaTaylor <taylorolivia.business@gmail.com>
2025-04-09 12:54:35 +0100

    run dx in release for both release and debug
    this is to make it work on olivia's laptop
    som edriver issue with dx11

--- src/main.cpp
+++ src/main.cpp
@@ -172,7 +172,7 @@ bool CreateD3D11Device(HWND hWnd)
 
     UINT createDeviceFlags = 0;
 #ifdef _DEBUG
-    createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+    //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
     D3D_FEATURE_LEVEL featureLevel;
